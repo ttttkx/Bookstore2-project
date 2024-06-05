e7eb7266c4bd3a4ef02d66277262b530166b6967
jjhengxin <144665728+jjhengxin@users.noreply.github.com>
2024-06-05 14:05:50 +0800

    Update buyer.py

--- be/model/buyer.py
+++ be/model/buyer.py
@@ -125,6 +125,133 @@ class Buyer(db_conn.DBConn):
             return 530, "{}".format(str(e))
 
         return 200, "ok"
+
+    # 查看历史订单
+    def get_buyer_orders(self, user_id: str) -> (int, str, list):
+        try:
+            orders = self.conn.query(NewOrder).filter_by(user_id=user_id).all()
+            buyer_orders = []
+            for i in orders:
+                buyer_orders.append(
+                    {'store_id': i.store_id,
+                     'order_id': i.order_id,
+                     'status': i.status}
+                )
+            return 200, "ok", buyer_orders
+        except BaseException as e:
+            return 530, "{}".format(str(e)), []
+        
+    # 确认收货
+    def receive_order(self, user_id: str, order_id: str) -> (int, str):
+        try:
+            order_data = self.conn.query(NewOrder).filter_by(order_id=order_id).first()
+
+            if order_data is None:
+                return error.error_invalid_order_id(order_id)
+
+            if order_data.user_id != user_id:
+                return error.error_authorization_fail()
+            if order_data.status == "received":
+                return 200, "Order is already received"
+            if order_data.status != "shipped":
+                return error.error_status_fail(order_id)
+
+            # 更新订单状态为 "received" 并记录收货时间
+            order_data.status = "received"
+            order_data.received_at = datetime.now().isoformat()
+            
+            self.conn.commit()
+        except BaseException as e:
+            return 530, "{}".format(str(e))
+
+        return 200, "ok"
+
+    # 取消订单
+    def cancel_order(self, user_id: str, order_id: str) -> (int, str):
+        try:
+            order_data = self.conn.query(NewOrder).filter_by(order_id=order_id).first()
+
+            if order_data is None:
+                return error.error_invalid_order_id(order_id)
+            if order_data.user_id != user_id:
+                return error.error_authorization_fail()
+            if order_data.status == "shipped" or order_data.status == "received":
+                return error.error_status_fail(order_id)
+            if order_data.status == "cancelled":
+                return 200, "Order is already cancelled."
+            if order_data.status == "paid":
+                # 获取订单详细信息
+                total_price = 0
+                order_details = self.conn.query(NewOrderDetail).filter_by(order_id=order_id).all()
+                for order_detail in order_details:
+                    count = order_detail.count
+                    price = order_detail.price
+                    total_price += price * count
+
+                user_data = self.conn.query(UserModel).filter_by(user_id=user_id).first()
+                if user_data is None:
+                    return error.error_non_exist_user_id(user_id)
+                # 计算退款金额
+                refund_amount = total_price
+                current_balance = user_data.balance
+                new_balance = current_balance + refund_amount
+                # 更新用户余额
+                user_data.balance = new_balance
+
+            # 取消订单，更新状态为 "cancelled"
+            order_data.status = "cancelled"
+            
+            self.conn.commit()
+        except BaseException as e:
+            return 530, "{}".format(str(e))
+        return 200, "ok"
+
+    def return_purchase(self, user_id: str, order_id: str) -> (int, str):
+        try:
+            order_data = self.conn.query(NewOrder).filter_by(order_id=order_id).first()
+
+            if order_data is None:
+                return error.error_invalid_order_id(order_id)
+            if order_data.user_id != user_id:
+                return error.error_authorization_fail()   
+            if order_data.status == "returned":
+                return 200, "Order is already returned."
+            if order_data.status != "received":
+                return error.error_status_fail(order_id)
+            #计算时间差
+            received_at_str = order_data.received_at  
+            received_at_dt = datetime.fromisoformat(received_at_str)  
+            now_dt = datetime.now()  
+            time_difference = now_dt - received_at_dt  
+  
+            # 检查时间差是否超过70秒  
+            if time_difference.total_seconds() > 70:  
+                return error.error_status_fail(order_id) 
+            # 获取订单详细信息
+            total_price = 0
+            order_details = self.conn.query(NewOrderDetail).filter_by(order_id=order_id).all()
+            for order_detail in order_details:
+                count = order_detail.count
+                price = order_detail.price
+                total_price += price * count
+
+            # 更新用户余额，将付款退还给用户
+            user_data = self.conn.query(UserModel).filter_by(user_id=user_id).first()
+            if user_data is None:
+                return error.error_non_exist_user_id(user_id)
+
+            # 计算退款金额
+            refund_amount = total_price
+            current_balance = user_data.balance
+            new_balance = current_balance + refund_amount
+            
+            user_data.balance = new_balance
+            order_data.status = "returned"
+            
+            self.conn.commit()
+        except BaseException as e:
+            return 530, "{}".format(str(e))
+        return 200, "ok"
     
 
 
