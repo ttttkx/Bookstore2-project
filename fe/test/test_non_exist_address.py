3d3eaa1df0512102e3aea828d325f69afe079116
ttttkx <144672418+ttttkx@users.noreply.github.com>
2024-06-06 14:05:15 +0800

    Add files via upload

---
+++ fe/test/test_non_exist_address.py
@@ -0,0 +1,52 @@
+import pytest
+
+from fe.test.gen_book_data import GenBook
+from fe.access.new_buyer import register_new_buyer
+import uuid
+from fe.access.auth import Auth
+from fe.access import auth
+from fe import conf
+import random
+
+provinces = ['北京', '天津', '河北', '山西', '内蒙古', '辽宁', '吉林', '黑龙江', '上海', '江苏',
+             '浙江', '安徽', '福建', '江西', '山东', '河南', '湖北', '湖南', '广东',
+             '广西', '海南', '重庆', '四川', '贵州', '云南', '西藏', '陕西', '甘肃',
+             '青海', '宁夏', '新疆', '台湾', '香港', '澳门']
+
+
+class TestNewOrder:
+    @pytest.fixture(autouse=True)
+    def pre_run_initialization(self):
+        self.auth = auth.Auth(conf.URL)
+        self.seller_id = "test_new_order_seller_id_{}".format(str(uuid.uuid1()))
+        self.store_id = "test_new_order_store_id_{}".format(str(uuid.uuid1()))
+        self.buyer_id = "test_new_order_buyer_id_{}".format(str(uuid.uuid1()))
+        self.password = self.seller_id
+        self.buyer = register_new_buyer(self.buyer_id, self.password)
+        self.gen_book = GenBook(self.seller_id, self.store_id)
+        yield
+
+        
+    def test_non_exist_address(self):       
+        ok, buy_book_id_list = self.gen_book.gen(
+            non_exist_book_id=False, low_stock_level=False
+        )
+        assert ok
+        code, _ = self.buyer.new_order(self.store_id, buy_book_id_list)
+        assert code != 200
+        
+    def test_save_address(self):                    
+        ok, buy_book_id_list = self.gen_book.gen(
+            non_exist_book_id=False, low_stock_level=False
+        )
+        assert ok
+        code, _ = self.buyer.new_order(self.store_id, buy_book_id_list)
+        assert code != 200
+        
+        self.address = random.choice(provinces)           
+        self.auth.set_address(self.buyer_id, self.address)
+        
+        code, _ = self.buyer.new_order(self.store_id, buy_book_id_list)
+        assert code == 200
+        
+        
